e12d59bcd69ea914cc4bca7c44677cda846d39e3
Ziad AK <ziadakgit@gmail.com>
2024-08-18 18:07:27 +0300

    update code and made it more random

--- mixing.py
+++ mixing.py
@@ -25,22 +25,24 @@ def calculate_bpm(audio_file):
     return tempo
 
 
-def crossfade_audio(first_file, second_file, output_file_path, crossfade_duration):
+def crossfade_audio(first_file, second_file, output_file_path, crossfade_duration, curve):
     command = [
         'ffmpeg', '-i', first_file, '-i', second_file, '-filter_complex',
-        f'[0][1]acrossfade=d={crossfade_duration}:c1=esin:c2=esin', '-c:a', 'libmp3lame','-b:a', '320k', output_file_path
+        f'[0][1]acrossfade=d={crossfade_duration}:c1={curve}:c2={curve}', '-c:a', 'libmp3lame','-b:a', '320k', output_file_path
     ]
     subprocess.run(command, check=True, stdout=subprocess.DEVNULL, stderr = subprocess.DEVNULL)
 
 
 def process_directory(directory, output_directory, bpm_tolerance, crossfade_duration,genre_chosen):
     song_list = [os.path.join(directory, f) for f in os.listdir(directory) if f.endswith('.mp3')]
+    curves = ["tri", "exp", "log", "hsin", "qsin", "nofade"]
     random.shuffle(song_list)  # Shuffle the list to be random
     if not os.path.exists(output_directory):
         print("Creating the final directory!")
         os.makedirs(output_directory)
     output_file = os.path.join(output_directory, f'{genre_chosen}_final_mix.mp3')
     temp_output = song_list[0]
+    bpm_temp = []
     for i in range(1, len(song_list)):
         song1 = temp_output
         song2 = song_list[i]
@@ -48,19 +50,35 @@ def process_directory(directory, output_directory, bpm_tolerance, crossfade_dura
         bpm2 = calculate_bpm(song2)
         # BPM check!
         if abs(bpm1 - bpm2) <= bpm_tolerance:
-            print(f"✔️ BPM Match ✔️ - Combining {song1} and {song2}")
+            curve = random.choice(curves)
+            print(f"✔️ BPM Match ✔️ - Combining {song1} and {song2} with {curve} curve")
             output_temp_file = os.path.join(output_directory, f'temp_{i}.mp3')
-            crossfade_audio(temp_output, song2, output_temp_file, crossfade_duration)
+            crossfade_audio(temp_output, song2, output_temp_file, crossfade_duration, curve)
             temp_output = output_temp_file
         else:
+            bpm_temp.append(song2)
             print(f"✖️ Skipped Combining ✖️ - {song1} and {song2} don't match!")
-    print("Deleting all temp files...")
-    os.rename(temp_output, output_file)
+
+    if not os.listdir(output_directory).__contains__(output_file):
+        os.rename(temp_output, output_file)
+    else:
+        os.remove(output_file)
+        os.rename(temp_output, output_file)
+
+    if bpm_temp.__len__() > 0:
+        temp_output = output_file
+        for i in range(0, len(bpm_temp)):
+            song2 = song_list[i]
+            output_temp_file = os.path.join(output_directory, f'temp_{i}.mp3')
+            crossfade_audio(temp_output, song2, output_temp_file, crossfade_duration, "tri")
+            temp_output = output_temp_file
+
+    print("🗑️ - Deleting all temp files...")
     for i in os.listdir(output_directory):
         if i.__contains__('temp_'):
             os.remove(f'{output_directory}\\{i}')
     print(f"✅ - Mix Created Successfully - ✅\n📂 - Output File: {output_file}")
 
-
-process_directory(ig.input_dir, ig.output_dir, bpm_tolerance=30, crossfade_duration=4, genre_chosen=ig.genre)
+genre = input("Please Enter Your Genre: ")
+process_directory(ig.input_dir, ig.output_dir, bpm_tolerance=9, crossfade_duration=4, genre_chosen=genre)
 
